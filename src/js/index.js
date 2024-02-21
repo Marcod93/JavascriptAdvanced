09ef45411a577079c1cf3c4b0ca81011d5a087aa
Marco <marcodonati93@libero.it>
2024-02-21 18:23:02 +0100

    Aggiunto img e creato inpout di ricerca

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery';
 import 'popper.js';
 import backgroundImage from '../img/books.jpg';
+
+document.body.style.backgroundImage = `url(${backgroundImage})`;
+document.body.style.backgroundRepeat = 'no-repeat';
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+document.body.style.backgroundBlendMode = "overlay";
